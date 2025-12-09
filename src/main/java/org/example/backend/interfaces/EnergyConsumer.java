5e009b659f81ddf0367b62a30a9e1e9b82953148
Tasnim Ouaer <ouaertasnim@tbs.u-tunis.tn>
2025-12-09 12:40:39 +0100

    Fixes

--- src/main/java/org/example/backend/interfaces/EnergyConsumer.java
+++ src/main/java/org/example/backend/interfaces/EnergyConsumer.java
@@ -3,4 +3,6 @@ package org.example.backend.interfaces;
 public interface EnergyConsumer {
     double getPowerRating();
     long getOnDurationSeconds();
+    double getEnergyConsumption();
+    double calculateEnergyUsage(double hours);
 }
